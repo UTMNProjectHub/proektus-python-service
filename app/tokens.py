19600162fa3a4434b8ce352c787f2887b009c88b
Subaruuwu <fylhtq2004.1yande@yandex.ru>
2025-06-09 15:31:14 +0500

    method to get credentials added

--- app/tokens.py
+++ app/tokens.py
@@ -6,10 +6,6 @@ def get_s3_credentials():
     secret_key = os.getenv("S3_SECRET_KEY")
     endpoint = os.getenv("S3_ENDPOINT")
     bucket_name = os.getenv("S3_BUCKET_NAME")
-    print(access_key)
-    print(secret_key)
-    print(endpoint)
-    print(bucket_name)
 
 
     if not all([access_key, secret_key, endpoint, bucket_name]):
